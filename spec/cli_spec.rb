115f3785061666ae3f06bc49213877582eec93b5
wimbleclaw <aisha305@hotmail.co.uk>
2019-03-01 00:15:38 +0000

    added  more tests

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -5,10 +5,6 @@ require_relative '../lib/product'
 
 describe Cli do
 
-    it 'initializes with @total at 0' do
-        expect(@cli.total).to eq 0
-    end
-
     it 'initializes with initial_arguments as the initialize arguements' do
         expect(@cli.initial_arguments).to eq [@set_users_coins, @set_products, @set_vending_machine_coins]
     end
@@ -25,8 +21,8 @@ describe Cli do
         expect(@cli.vending_machine.products.map { |product| {product.name => {product.price[0] => product.value[0]}}}).to eq [{"Kettle crips"=>{"£1"=>100}},{"Walkers crips"=>{"50p"=>50}},{"Popchips"=>{"65p"=>65}},{"Sensations"=>{"£1.50"=>150}},{"Quavers"=>{"80p"=>80}}]
     end
 
-    it 'outputs a line separator' do
-        expect{@cli.separate}.to output("------------------------------------\n").to_stdout
+    it 'outputs a divider' do
+        expect{@cli.display_divider}.to output("------------------------------------\n").to_stdout
     end
 
     it 'outputs a greeting' do
@@ -34,16 +30,7 @@ describe Cli do
     end
 
     it 'list to out all the coins followed by a separator' do
-        expect{@cli.list_coins}.to output("You have:\n£2 £1 50p 20p 10p 5p 2p 1p\n------------------------------------\n").to_stdout
-    end
-
-    it 'lists out all the menu options' do
-        expect{@cli.main_menu_options}.to output("What would you like to do?\n(1) Choose a snack\n(2) Reset your balance\n(3) Reset vending machine\n(X) Exit\n").to_stdout
-    end
-
-    it 'set_total adds up the input value' do
-        @cli.input_value = [1, 2, 3]
-        expect(@cli.set_total).to eq 6
+        expect{@cli.list_user_coins}.to output("You have:\n£2 £1 50p 20p 10p 5p 2p 1p\n------------------------------------\n").to_stdout
     end
 
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -7,6 +7,10 @@ describe VendingMachine do
 
     let(:vending_machine) {VendingMachine.new(set_products, set_vending_machine_coins)}
 
+    it 'initializes with @total_amount_paid at 0' do
+        expect(vending_machine.total_amount_paid).to eq 0
+    end
+
     it 'initializes the vending machine with the correct products, price and value' do
         expect(vending_machine.products.map { |product| {product.name => {product.price[0] => product.value[0]}}}).to eq [{"Kettle crips"=>{"£1"=>100}},{"Walkers crips"=>{"50p"=>50}},{"Popchips"=>{"65p"=>65}},{"Sensations"=>{"£1.50"=>150}},{"Quavers"=>{"80p"=>80}}]
     end
@@ -39,19 +43,25 @@ describe VendingMachine do
     end
 
     it 'list_products to puts "Our products are:"' do
-        expect{vending_machine.list_products}.to output("Our products are:\n").to_stdout
-    end
-
-    it 'returns a list of the products and their price"' do
-        expect(vending_machine.list_products).to eq ["Kettle crips, £1", "Walkers crips, 50p", "Popchips, 65p", "Sensations, £1.50", "Quavers, 80p"]
+        expect{vending_machine.list_available_products}.to output("Our products are: \nKettle crips, £1\nWalkers crips, 50p\nPopchips, 65p\nSensations, £1.50\nQuavers, 80p\n").to_stdout
     end
 
     it 'removes a product' do
-        expect(vending_machine.remove_product(vending_machine.products.first).map{|product| product.name}).to eq ["Walkers crips", "Popchips", "Sensations", "Quavers"]
+        vending_machine.selected_product = vending_machine.products.first
+        expect((vending_machine.remove_product).map{|product| product.name}).to eq ["Walkers crips", "Popchips", "Sensations", "Quavers"]
     end
 
     it 'removes a coin' do
         expect(vending_machine.remove_vending_machine_coin(vending_machine.vending_machine_coins.first).map{|coin| coin.type}).to eq ["£2", "£2", "£1", "£1", "£1", "50p", "50p", "50p", "20p", "20p", "20p", "10p", "10p", "10p", "5p", "5p", "5p", "2p", "2p", "2p", "1p", "1p", "1p"]
+    end
+
+    it 'has all the menu options' do
+        expect(vending_machine.main_menu_options).to eq ({"1"=>"Choose a snack", "2"=>"Reset your balance", "3"=>"Reset vending machine", "X"=>"Exit"})
+    end
+
+    it 'set_total adds up the input value' do
+        vending_machine.paid_value = [1, 2, 3]
+        expect(vending_machine.set_total_paid).to eq 6
     end
 
 end
